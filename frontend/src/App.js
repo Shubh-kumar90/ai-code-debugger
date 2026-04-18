6c5cd6f54e21f33fc0f6e04071f5ea2042a6db1b
Shubham Prajapati <shubh.prajapati90@gmail.com>
2026-04-18 18:02:08 +0530

    fixed missing useRef import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Editor from "@monaco-editor/react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
